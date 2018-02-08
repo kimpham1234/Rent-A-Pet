d07f6797299a16f906e8b74628051e6844795396
kimpham1234 <kim.g.pham@sjsu.edu>
2018-02-07 17:04:20 -0800

    view pet detail

--- app.js
+++ app.js
@@ -9,12 +9,14 @@ app.use(express.static("public"));
 app.set("view engine", "ejs");
 
 var pets = [
-	{name: "Wacky", img: "http://3dogmedia.com/wp-content/uploads/2016/08/120251710-632x3531.jpg"},
-	{name: "Bacon", img: "https://boulderinn.com/bldrinn/wp-content/uploads/2010/10/pets-712-X-495.jpg"},
-	{name: "Kiwi", img: "https://timedotcom.files.wordpress.com/2017/11/dogs-cats-brain-study.jpg"},
-	{name: "Bambu", img: "http://saveapetil.org/wp-content/uploads/2018/01/310374456.jpg"},
-	{name: "Pixie", img: "http://www.bestfriendspetcare.com/wp-content/uploads/2011/04/111.jpg"}
+	{id: 0, name: "Wacky", img: "http://3dogmedia.com/wp-content/uploads/2016/08/120251710-632x3531.jpg"},
+	{id: 1, name: "Bacon", img: "https://boulderinn.com/bldrinn/wp-content/uploads/2010/10/pets-712-X-495.jpg"},
+	{id: 2, name: "Kiwi", img: "https://timedotcom.files.wordpress.com/2017/11/dogs-cats-brain-study.jpg"},
+	{id: 3, name: "Bambu", img: "http://saveapetil.org/wp-content/uploads/2018/01/310374456.jpg"},
+	{id: 4, name: "Pixie", img: "http://www.bestfriendspetcare.com/wp-content/uploads/2011/04/111.jpg"}
 ]
+
+var id = 4;
 
 app.get('/', function(req, res){
 	res.render("home", {pets: pets});
@@ -28,9 +30,16 @@ app.get('/pet/new', function(req, res){
 app.post('/pet/new', function(req, res){
 	var petName = req.body.petName;
 	var url = req.body.picUrl;
-	pets.push({name: petName, img: url});
+	id++;
+	pets.push({id: id, name: petName, img: url});
 	res.redirect('/');
 })
+
+app.get('/pet/:id', function(req, res){
+	var petId = Number(req.params.id);
+	var pet = pets[petId];
+	res.render("petDetail", {pet: pet});
+});
 
 app.listen(3000, function(){
     console.log("Application starting at port "+3000); 
